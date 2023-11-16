3d6324e14d9d085d908aeb54e33de6d23f670812
Ubuntu <ubuntu@ip-172-31-35-187.eu-west-1.compute.internal>
2023-11-16 12:42:46 +0000

    premier ajout

---
+++ lufth_o_api/request.py
@@ -0,0 +1,27 @@
+import requests
+# import lufth_o_api.authentication as auth
+import authentication as auth
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='luth_o_api.log',
+    filemode='a',
+    format='%(asctime)s -%(levelname)s- %(message)s')
+
+
+def make_request(url):
+    """
+    faire un request au URL
+    :param url:
+    :return json object:
+    """
+    header = auth.get_header()
+    r = requests.get(url, headers=header)
+    if r.status_code == 200:
+        logging.info('request to URL ' + url + ' status: ' + str(r.status_code))
+        return r.text
+    else:
+        logging.info('request to URL ' + url + ' status: ' + str(r.status_code) + ' message: '+r.text)
+        return 'invalid request'
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Lufth OAPI',
+      version='0.1',
+      description='Lufthansa OAPI python',
+      url='https://github.com/',
+      author='Ahmed SAID',
+      author_email='ahmed.nejib.said@gmail.com',
+      license='GPL-2.0',
+      packages=['lufth_o_api'],
+      zip_safe=False)
